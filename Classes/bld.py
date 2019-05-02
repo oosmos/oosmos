1a2d45324e01d69d0fb22e9bae5c14d5160ac749
zzglenm <mglenn@cox.net>
2019-05-02 15:44:00 -0700

    add toggle_objectthread.c class

--- Classes/bld.py
+++ Classes/bld.py
@@ -1,4 +1,4 @@
 import os
 
 os.system(r'..\Gen\gen.exe classes.json')
-os.system(r'cl -W4 -c -D_CRT_SECURE_NO_WARNINGS pid.c sock.c dns.c matrix.c keyer.c toggle.c toggle_state.c accum.c keypad.c pin.c reg.c prt.c tok.c -I..\source')
+os.system(r'cl -W4 -c -D_CRT_SECURE_NO_WARNINGS pid.c sock.c dns.c matrix.c keyer.c toggle.c toggle_state.c toggle_objectthread.c accum.c keypad.c pin.c reg.c prt.c tok.c -I..\source')
---
+++ Classes/toggle_objectthread.c
@@ -0,0 +1,73 @@
+//
+// OOSMOS toggle Class
+//
+// Copyright (C) 2014-2019  OOSMOS, LLC
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 of the License ("GPLv2").
+//
+// This software may be used without the GPLv2 restrictions by entering
+// into a commercial license agreement with OOSMOS, LLC.
+// See <https://www.oosmos.com/licensing/>.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef toggleMAX
+#define toggleMAX 5
+#endif
+
+//===================================
+
+#include "oosmos.h"
+#include "toggle.h"
+#include "pin.h"
+#include <stdint.h>
+#include <stddef.h>
+
+struct toggleTag
+{
+  oosmos_sObjectThread m_ObjectThread;
+
+  pin      * m_pPin;
+  uint32_t   m_TimeOnMS;
+  uint32_t   m_TimeOffMS;
+};
+
+static void ToggleThread(void * pObject, oosmos_sState * pState)
+{
+  oosmos_POINTER_GUARD(pObject);
+  oosmos_POINTER_GUARD(pState);
+
+  toggle * pToggle = (toggle *) pObject;
+
+  oosmos_ThreadBegin();
+    for (;;) {
+      pinOn(pToggle->m_pPin);
+      oosmos_ThreadDelayMS(pToggle->m_TimeOnMS);
+
+      pinOff(pToggle->m_pPin);
+      oosmos_ThreadDelayMS(pToggle->m_TimeOffMS);
+    }
+  oosmos_ThreadEnd();
+}
+
+extern toggle * toggleNew(pin * pPin, uint32_t TimeOnMS, uint32_t TimeOffMS)
+{
+  oosmos_Allocate(pToggle, toggle, toggleMAX, NULL);
+
+  oosmos_ObjectThreadInit(pToggle, m_ObjectThread, ToggleThread);
+
+  pToggle->m_pPin      = pPin;
+  pToggle->m_TimeOnMS  = TimeOnMS;
+  pToggle->m_TimeOffMS = TimeOffMS;
+
+  return pToggle;
+}
