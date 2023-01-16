9e70c2d8d8ffb544718b58848d944ffa7dd052e9
Mark Glenn <mglenn@cox.net>
2023-01-16 08:04:19 -0700

    Add OrthoThreadEvents.c and OrthoThreadStates.c in Examples

---
+++ Examples/Ortho/Windows/OrthoThreadEvents.c
@@ -0,0 +1,152 @@
+#include "oosmos.h"
+#include "OrthoThreads.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef union {
+  oosmos_sEvent Base;
+} uEvents;
+
+//>>>EVENTS
+enum {
+  evTick = 1
+};
+
+#ifdef oosmos_DEBUG
+  static const char * OOSMOS_EventNames(int EventCode)
+  {
+    switch (EventCode) {
+      case evTick: return "evTick";
+      default: return "";
+    }
+  }
+#endif
+//<<<EVENTS
+
+struct orthoTag
+{
+//>>>DECL
+  oosmos_sStateMachine(ROOT, uEvents, 3);
+    oosmos_sOrtho Outer_State;
+      oosmos_sOrthoRegion Outer_Region1_State;
+        oosmos_sLeaf Outer_Region1_Metronome_State;
+      oosmos_sOrthoRegion Outer_Region2_State;
+        oosmos_sLeaf Outer_Region2_First_State;
+      oosmos_sOrthoRegion Outer_Region3_State;
+        oosmos_sLeaf Outer_Region3_Second_State;
+//<<<DECL
+};
+
+static void MetronomeThread(ortho * pOrtho, oosmos_sState * pState)
+{
+  oosmos_UNUSED(pOrtho);
+
+  oosmos_ThreadBegin();
+    for (;;) {
+      oosmos_ThreadDelayMS(2000);
+	  oosmos_PushEventCode(pOrtho, evTick);
+    }
+  oosmos_ThreadEnd();
+}
+
+static void FirstThread(ortho * pOrtho, oosmos_sState * pState)
+{
+  oosmos_UNUSED(pOrtho);
+
+  oosmos_ThreadBegin();
+    for (;;) {
+      oosmos_ThreadWaitEvent(evTick);
+	  printf("FirstThread: Got evTick\n");
+    }
+  oosmos_ThreadEnd();
+}
+
+static void SecondThread(ortho * pOrtho, oosmos_sState * pState)
+{
+  oosmos_UNUSED(pOrtho);
+
+  oosmos_ThreadBegin();
+    for (;;) {
+      oosmos_ThreadWaitEvent(evTick);
+	  printf("SecondThread: Got evTick\n");
+    }
+  oosmos_ThreadEnd();
+}
+
+//>>>CODE
+// SR28
+static bool Outer_Region1_Metronome_State_Code(void * pObject, oosmos_sState * pState, const oosmos_sEvent * pEvent)
+{
+  ortho * pOrtho = (ortho *) pObject;
+
+  switch (oosmos_EventCode(pEvent)) {
+    case oosmos_POLL: {
+      MetronomeThread(pOrtho, pState);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// SR28
+static bool Outer_Region2_First_State_Code(void * pObject, oosmos_sState * pState, const oosmos_sEvent * pEvent)
+{
+  ortho * pOrtho = (ortho *) pObject;
+
+  switch (oosmos_EventCode(pEvent)) {
+    case oosmos_POLL: {
+      FirstThread(pOrtho, pState);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// SR28
+static bool Outer_Region3_Second_State_Code(void * pObject, oosmos_sState * pState, const oosmos_sEvent * pEvent)
+{
+  ortho * pOrtho = (ortho *) pObject;
+
+  switch (oosmos_EventCode(pEvent)) {
+    case oosmos_POLL: {
+      SecondThread(pOrtho, pState);
+      return true;
+    }
+  }
+
+  return false;
+}
+//<<<CODE
+
+extern ortho * orthoNew(void)
+{
+  oosmos_Allocate(pOrtho, ortho, 1, NULL);
+
+//>>>INIT
+  oosmos_StateMachineInit(pOrtho, ROOT, NULL, Outer_State);
+    oosmos_OrthoInit(pOrtho, Outer_State, ROOT, NULL);
+      oosmos_OrthoRegionInit(pOrtho, Outer_Region1_State, Outer_State, Outer_Region1_Metronome_State, NULL);
+        oosmos_LeafInit(pOrtho, Outer_Region1_Metronome_State, Outer_Region1_State, Outer_Region1_Metronome_State_Code);
+      oosmos_OrthoRegionInit(pOrtho, Outer_Region2_State, Outer_State, Outer_Region2_First_State, NULL);
+        oosmos_LeafInit(pOrtho, Outer_Region2_First_State, Outer_Region2_State, Outer_Region2_First_State_Code);
+      oosmos_OrthoRegionInit(pOrtho, Outer_Region3_State, Outer_State, Outer_Region3_Second_State, NULL);
+        oosmos_LeafInit(pOrtho, Outer_Region3_Second_State, Outer_Region3_State, Outer_Region3_Second_State_Code);
+
+  oosmos_Debug(pOrtho, OOSMOS_EventNames);
+//<<<INIT
+
+  return pOrtho;
+}
+
+extern int main(void)
+{
+  (void) orthoNew();
+
+  for (;;) {
+    oosmos_RunStateMachines();
+    oosmos_DelayMS(1);
+  }
+}
--- Examples/Ortho/Windows/bld.py
+++ Examples/Ortho/Windows/bld.py
@@ -10,7 +10,9 @@ oosmos_c = oosmos_dir+r'\Source\oosmos.c'
 
 os.system(oosmos_dir + r'\Gen\gen.exe ortho.json')
 
-oosmos.cWindows.Compile(oosmos_dir, ['Motion.c', oosmos_c],     '-Doosmos_ORTHO')
-oosmos.cWindows.Compile(oosmos_dir, ['Completion.c', oosmos_c], '-Doosmos_ORTHO')
-oosmos.cWindows.Compile(oosmos_dir, ['EnterExit.c', oosmos_c],  '-Doosmos_ORTHO')
-oosmos.cWindows.Compile(oosmos_dir, ['OrthoThreads.c', oosmos_c],  '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['OrthoThreadEvents.c', oosmos_c], '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['OrthoThreadStates.c', oosmos_c], '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['Motion.c', oosmos_c],            '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['Completion.c', oosmos_c],        '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['EnterExit.c', oosmos_c],         '-Doosmos_ORTHO')
+oosmos.cWindows.Compile(oosmos_dir, ['OrthoThreads.c', oosmos_c],      '-Doosmos_ORTHO')
